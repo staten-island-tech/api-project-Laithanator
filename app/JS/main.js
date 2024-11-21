c1cd97730b6464d4fc9a21b558e8dc3e16de471f
Laithanator <144480666+Laithanator@users.noreply.github.com>
2024-11-21 14:02:16 -0500

    65j4jk7k

--- app/JS/main.js
+++ app/JS/main.js
@@ -16,11 +16,15 @@ async function getData() {
 }
 getData();
 
-let myMap = L.map(".map").setView([37.61, -122.011], 10);
+data = 
 
-L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-  // Attribution is obligatory as per copyright!
-  maxZoom: 20,
-}).addTo(myMap);
+function run(){
+  statisticOne = response.
+}
+
+function cardCreate(array) {
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeEnd",
+
+  );
+}
